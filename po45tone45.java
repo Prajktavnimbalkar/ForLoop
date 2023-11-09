ec963ffa7ac7f2416f3af948e7b64498b2a8523d
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-11-09 09:19:00 +0530

    Add files via upload

---
+++ po45tone45.java
@@ -0,0 +1,29 @@
+package forloop;
+
+public class po45tone45 {
+	public static void main(String[] args) {
+        int start = 45;
+        int end = -45;
+        
+        if (start > end) {
+            for (int i = start; i >= end; i--) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = start; i <= end; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+ 
+
+
+
+
+
+
+
+
